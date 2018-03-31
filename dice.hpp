e7fff3a8d471fc5961db2a931c8904f22d43a3aa
chen-charles <chen-charles@users.noreply.github.com>
2018-03-30 20:01:47 -0400

    dice allowing seeds

--- dice.hpp
+++ dice.hpp
@@ -8,14 +8,19 @@ class Dice
 {
 public:
 	virtual unsigned int roll() const = 0;
+	void seed(unsigned int sd)
+	{
+		eng.seed(sd);
+	}
+
+private:
+	static std::mt19937 eng;
 
 protected:
 	// lower and upper are both INCLUSIVE
 	// random number generation utility
 	static unsigned int ranged_rand(unsigned int lower, unsigned int upper)
 	{
-		std::random_device rd;
-		std::mt19937 eng(rd());
 		std::uniform_int_distribution<> distr(lower, upper);
 		return distr(eng);
 	}
